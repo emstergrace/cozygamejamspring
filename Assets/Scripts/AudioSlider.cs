7a88639e887e7590127d520e09389ea21d48b2c2
Vanhoras <phielsche@t-online.de>
2023-04-30 15:29:01 +0200

    Audio slider persists between scenes

--- Assets/Scripts/AudioSlider.cs
+++ Assets/Scripts/AudioSlider.cs
@@ -1,11 +1,22 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class AudioSlider : MonoBehaviour
 {
 
     [SerializeField]
     private AudioMixer audioMixer;
+
+    private void Awake()
+    {
+        Slider slider = gameObject.GetComponent<Slider>();
+
+        float currentVolume;
+        audioMixer.GetFloat("MusicVolume", out currentVolume);
+
+        slider.value = Mathf.Pow(10, currentVolume / 20);
+    }
 
     public void SetVolume(float sliderValue)
     {
